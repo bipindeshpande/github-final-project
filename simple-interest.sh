bb02219f3881ea75771761099c331adddd2276bc
bipindeshpande <147257990+bipindeshpande@users.noreply.github.com>
2023-10-07 15:29:20 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # bipin_deshpande@outlook.com
    # Input:
    # p, principal amount
    # t, time period in years
